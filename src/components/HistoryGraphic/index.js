22b4eccae160c930ecf4f419b28ebe1b37c395fb
andreluissousa <andreborges1777@gmail.com>
2023-10-31 23:28:22 -0300

    Estilizando gráfico

--- src/components/HistoryGraphic/index.js
+++ src/components/HistoryGraphic/index.js
@@ -15,26 +15,35 @@ export function HistoryGraphic(props) {
                   }
                 ]
               }}
-                width={Dimensions.get("window").width} // from react-native
-                height={220}
-                yAxisLabel="$"
-                withVerticalLines={false}
-                yLabelsOffset={1}
-                withVerticalLabels={false}
-                chartConfig={{
-                    backgroundColor: "#000000",
-                    backgroundGradientFrom: "#1E2923",
-                    backgroundGradientTo: "#08130D",
-                    backgroundGradientToOpacity: 0.5,
-                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,                    
-                    propsForDots: {
-                        r: "1",
-                        strokeWidth: "1",
-                        stroke: "#ffa726"
-                    }
-                }}
-                bezier
+              width={Dimensions.get("window").width} // from react-native
+              height={220}
+              yAxisLabel="$"
+              withVerticalLines={false}
+              yLabelsOffset={1}
+              withVerticalLabels={false}
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: "#000000",
+                backgroundGradientFrom: "#1E2923",
+                backgroundGradientTo: "#08130D",
+                backgroundGradientToOpacity: 0.5,
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16
+                },
+                propsForDots: {
+                  r: "1",
+                  strokeWidth: "1",
+                  stroke: "#ffa726"
+                }
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16
+              }}
             />
         </View>
     )
