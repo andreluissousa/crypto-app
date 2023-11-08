87f4954af24edfee9afad2555c3a799a53286241
andreluissousa <andreborges1777@gmail.com>
2023-11-07 22:27:07 -0300

    Configurando anúcios

--- src/components/CurrentPrice/index.js
+++ src/components/CurrentPrice/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+const adUnitId = TestIds.BANNER;
 
 export function CurrentPrice(props) {
     return(
         <View style={styles.headerPrice}>
+            <BannerAd 
+                unitId={adUnitId}
+                size={BannerAdSize}
+                requestOptions={{
+                requestNonPersonalizedAdsOnly: true,
+                }}
+            />
+            <Text style={styles.textPrice}>Anúncio</Text>
             <Text style={styles.currentPrice}>${props.lastCotation}</Text>
             <Text style={styles.textPrice}>Ultima Cotação do Dólar</Text>
         </View>
